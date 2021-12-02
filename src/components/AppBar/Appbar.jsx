a161276e8507b02f7fd9e8379a4f808642e2096b
Jose Manuel Rojas <jose.rojas@conekta.com>
2021-12-02 12:14:51 -0500

    adding appBar

---
+++ src/components/AppBar/Appbar.jsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import { Search, SearchIconWrapper, StyledInputBase } from './material.styles';
+
+export default function SearchAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+            onClick={()=> alert('Esta funcinalidad no está implementada')}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Escribe para buscar..."
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            OfesAuto
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
---
+++ src/material.styles.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#4fbc39',
+      contrastText: '#fff',
+    },
+    secondary: 
+    {
+      main: '#484848',
+    },
+    error: {
+      main: '#F44336',
+    },
+  },
+});
